7713530eef4ab4294f7ccdb75708bb92d29730de
Alena Fomenkova <fruitella30sep@mail.ru>
2020-07-10 11:54:35 +0300

    Adding default config.

---
+++ src/plugin/model/defaultConfig.ts
@@ -0,0 +1,13 @@
+const defaultConfig = {
+  min: 0,
+  max: 100,
+  step: 1,
+  currentValueFrom: 20,
+  currentValueTo: 70,
+  isVertical: false,
+  hasInterval: true,
+  hasPointer: true,
+  hasScale: true,
+};
+
+export default defaultConfig;
