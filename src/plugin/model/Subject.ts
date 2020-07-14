85742d32f0e2154a5c201e1a2e663ac0e13ffb55
Alena Fomenkova <fruitella30sep@mail.ru>
2020-07-14 12:28:23 +0300

    Adding subject.

---
+++ src/plugin/model/Subject.ts
@@ -0,0 +1,31 @@
+class Subject<T> {
+  private observers: ((value: T) => void) [] = [];
+  private value: T | undefined;
+
+  setValue(value: T) {
+    this.value = value;
+    this.observers.forEach((observer) => {
+      observer(value);
+    });
+  }
+
+  attach(observer: (value: T) => void) {
+    if (this.observers.includes(observer)) {
+      console.log('Observer already attached.');
+    } else {
+      this.observers.push(observer);
+    }
+  }
+
+  detach(observer: (value: T) => void) {
+    const index = this.observers.indexOf(observer);
+
+    if (index === -1) {
+      console.log('Observer not found.');
+    } else {
+      this.observers.splice(index, 1);
+    }
+  }
+}
+
+export default Subject;
