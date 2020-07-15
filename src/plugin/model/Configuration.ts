47ca30080141c663c042c6ef312d0153fa8228b4
Alena Fomenkova <fruitella30sep@mail.ru>
2020-07-15 13:33:18 +0300

    Adding configuration type.

---
+++ src/plugin/model/Configuration.ts
@@ -0,0 +1,13 @@
+type Configuration = {
+  min: number;
+  max: number;
+  step: number;
+  currentValueFrom: number;
+  currentValueTo: number;
+  isVertical: boolean;
+  hasInterval: boolean;
+  hasPointer: boolean;
+  hasScale: boolean;
+};
+
+export default Configuration;
