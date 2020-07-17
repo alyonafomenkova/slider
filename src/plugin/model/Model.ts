071ca43e5a304abb8375e6b9252e3b168263dc22
Alena Fomenkova <fruitella30sep@mail.ru>
2020-07-18 00:06:38 +0300

    Adding setters.

--- src/plugin/model/Model.ts
+++ src/plugin/model/Model.ts
@@ -13,6 +13,7 @@ class Model {
   private hasScale: Subject<boolean>;
 
   constructor(public configuration: Configuration) {
+    this.validateConfigaration(configuration);
     this.min = new Subject(configuration.min);
     this.max = new Subject(configuration.max);
     this.step = new Subject(configuration.step);
@@ -22,6 +23,27 @@ class Model {
     this.hasInterval = new Subject(configuration.hasInterval);
     this.hasPointer = new Subject(configuration.hasPointer);
     this.hasScale = new Subject(configuration.hasScale);
+  }
+
+  private validateConfigaration(configuration: Configuration): void {
+    if (configuration.min >= configuration.max) {
+      throw new Error(`Min must be less than max!`);
+    }
+    if (configuration.step <= 0) {
+      throw new Error(`Step must be more than 0!`);
+    }
+    if (configuration.step > configuration.max - configuration.min) {
+      throw new Error(`Step must be less than max - min!`);
+    }
+    if (configuration.currentValueFrom < configuration.min || configuration.currentValueFrom > configuration.max) {
+      throw new Error(`CurrentValueFrom must be less than max, but more than min!`);
+    }
+    if (configuration.currentValueTo < configuration.min || configuration.currentValueTo > configuration.max) {
+      throw new Error(`CurrentValueTo must be less than max, but more than min!`);
+    }
+    if (configuration.currentValueTo < configuration.currentValueFrom) {
+      throw new Error(`CurrentValueTo must be more than currentValueFrom!`);
+    }
   }
 
   attachMin(observer: (value: number) => void) {
@@ -44,21 +66,21 @@ class Model {
     this.currentValueTo.attach(observer);
   }
 
-  // attachOrientation(observer: (value: boolean) => void) {
-  //   this.isVertical.attach(observer);
-  // }
-  //
-  // attachInterval(observer: (value: boolean) => void) {
-  //   this.hasInterval.attach(observer);
-  // }
-  //
-  // attachPointer(observer: (value: boolean) => void) {
-  //   this.hasPointer.attach(observer);
-  // }
-  //
-  // attachScale(observer: (value: boolean) => void) {
-  //   this.hasScale.attach(observer);
-  // }
+  attachOrientation(observer: (value: boolean) => void) {
+    this.isVertical.attach(observer);
+  }
+
+  attachInterval(observer: (value: boolean) => void) {
+    this.hasInterval.attach(observer);
+  }
+
+  attachPointer(observer: (value: boolean) => void) {
+    this.hasPointer.attach(observer);
+  }
+
+  attachScale(observer: (value: boolean) => void) {
+    this.hasScale.attach(observer);
+  }
 
   setMin(value: number) {
     const max = this.max.getValue();
@@ -89,51 +111,88 @@ class Model {
     }
   }
 
-  // setMax(value: number) {
-  //   // TODO: доделать валидацию, тесты
-  //   const min = this.min.getValue();
-  //   value >= min ? this.max.setValue(value) : this.max.setValue(min);
-  // }
-  //
-  // setStep(value: number) {
-  //   // TODO: доделать валидацию, тесты
-  //   const min = this.min.getValue();
-  //   const max = this.max.getValue();
-  //   const step = this.step.getValue();
-  //   if (value >= min && value <= max) {
-  //     this.max.setValue(step);
-  //   }
-  // }
-  //
-  // setValueFrom(value: number) {
-  //   // TODO: доделать валидацию, тесты
-  //   this.currentValueFrom.setValue(value);
-  // }
-  //
-  // setValueTo(value: number) {
-  //   // TODO: доделать валидацию, тесты
-  //   this.currentValueTo.setValue(value);
-  // }
-  //
-  // setOrientation(value: boolean) {
-  //   // TODO: доделать валидацию, тесты
-  //   this.isVertical.setValue(value);
-  // }
-  //
-  // setInterval(value: boolean) {
-  //   // TODO: доделать валидацию, тесты
-  //   this.hasInterval.setValue(value);
-  // }
-  //
-  // setPointer(value: boolean) {
-  //   // TODO: доделать валидацию, тесты
-  //   this.hasPointer.setValue(value);
-  // }
-  //
-  // setScale(value: boolean) {
-  //   // TODO: доделать валидацию, тесты
-  //   this.hasScale.setValue(value);
-  // }
+  setMax(value: number) {
+    const min = this.min.getValue();
+    const from = this.currentValueFrom.getValue();
+    const to = this.currentValueTo.getValue();
+    const step = this.step.getValue();
+
+    if (value > to) {
+      this.max.setValue(value);
+    } else {
+      this.currentValueTo.setValue(value);
+      this.max.setValue(value);
+    }
+    if (value < from) {
+      this.currentValueFrom.setValue(value);
+    }
+    if (value > min && value - min < step) {
+      this.step.setValue(value - min);
+    }
+  }
+
+  setStep(value: number) {
+    if (value <= 0) {
+      throw new Error("Step must be > 0!");
+    }
+    const min = this.min.getValue();
+    const max = this.max.getValue();
+    this.step.setValue(max - min > value ? value : max - min);
+  }
+
+  setFrom(value: number) {
+    const max = this.max.getValue();
+    const step = this.step.getValue();
+    const to = this.currentValueTo.getValue();
+    const from = Math.round(value / step) * step;
+    const hasInterval = this.hasInterval.getValue();
+
+    if (hasInterval) {
+      if (value > to) {
+        this.currentValueFrom.setValue(to)
+      } else {
+        this.currentValueFrom.setValue(from);
+      }
+    } else {
+      if (value > max) {
+        this.currentValueFrom.setValue(max);
+      } else {
+        this.currentValueFrom.setValue(from);
+      }
+    }
+  }
+
+  setTo(value: number) {
+    const max = this.max.getValue();
+    const from = this.currentValueFrom.getValue();
+    const step =  this.step.getValue();
+    const to = Math.round(value / step) * step;
+
+    if (value < from) {
+      this.currentValueTo.setValue(from);
+    } else {
+      this.currentValueTo.setValue(to);
+    }
+    if (value > max) {
+      this.currentValueTo.setValue(max);
+    }
+  }
+
+  setVertical(value: boolean) {
+    this.isVertical.setValue(value);
+  }
+
+  setInterval(value: boolean) {
+    this.hasInterval.setValue(value);
+  }
+
+  setPointer(value: boolean) {
+    this.hasPointer.setValue(value);
+  }
+
+  setScale(value: boolean) {
+    this.hasScale.setValue(value);
+  }
 }
 
 export default Model;
--- src/plugin/tests/model/ModelTest.ts
+++ src/plugin/tests/model/ModelTest.ts
@@ -12,6 +12,168 @@ const testСonfiguration = {
   hasScale: true,
 };
 
+describe('Validate configaration', () => {
+  it('Min > max', () => {
+    const configuration = {
+      min: 150,
+      max: 100,
+      step: 10,
+      currentValueFrom: 20,
+      currentValueTo: 70,
+      isVertical: false,
+      hasInterval: true,
+      hasPointer: true,
+      hasScale: true,
+    };
+
+    expect( function(){ new Model(configuration); } ).toThrow(new Error(`Min must be less than max!`));
+  });
+
+  it('Min = max', () => {
+    const configuration = {
+      min: 100,
+      max: 100,
+      step: 10,
+      currentValueFrom: 20,
+      currentValueTo: 70,
+      isVertical: false,
+      hasInterval: true,
+      hasPointer: true,
+      hasScale: true,
+    };
+
+    expect( function(){ new Model(configuration); } ).toThrow(new Error(`Min must be less than max!`));
+  });
+
+  it('Step < 0', () => {
+    const configuration = {
+      min: 0,
+      max: 100,
+      step: -1,
+      currentValueFrom: 20,
+      currentValueTo: 70,
+      isVertical: false,
+      hasInterval: true,
+      hasPointer: true,
+      hasScale: true,
+    };
+
+    expect( function(){ new Model(configuration); } ).toThrow(new Error(`Step must be more than 0!`));
+  });
+
+  it('Step = 0', () => {
+    const configuration = {
+      min: 0,
+      max: 100,
+      step: 0,
+      currentValueFrom: 20,
+      currentValueTo: 70,
+      isVertical: false,
+      hasInterval: true,
+      hasPointer: true,
+      hasScale: true,
+    };
+
+    expect( function(){ new Model(configuration); } ).toThrow(new Error(`Step must be more than 0!`));
+  });
+
+  it('Step > max - min', () => {
+    const configuration = {
+      min: 0,
+      max: 100,
+      step: 110,
+      currentValueFrom: 20,
+      currentValueTo: 70,
+      isVertical: false,
+      hasInterval: true,
+      hasPointer: true,
+      hasScale: true,
+    };
+
+    expect( function(){ new Model(configuration); } ).toThrow(new Error(`Step must be less than max - min!`));
+  });
+
+  it('From < min', () => {
+    const configuration = {
+      min: 0,
+      max: 100,
+      step: 10,
+      currentValueFrom: -20,
+      currentValueTo: 70,
+      isVertical: false,
+      hasInterval: true,
+      hasPointer: true,
+      hasScale: true,
+    };
+
+    expect( function(){ new Model(configuration); } ).toThrow(new Error(`CurrentValueFrom must be less than max, but more than min!`));
+  });
+
+  it('From > max', () => {
+    const configuration = {
+      min: 0,
+      max: 100,
+      step: 10,
+      currentValueFrom: 120,
+      currentValueTo: 70,
+      isVertical: false,
+      hasInterval: true,
+      hasPointer: true,
+      hasScale: true,
+    };
+
+    expect( function(){ new Model(configuration); } ).toThrow(new Error(`CurrentValueFrom must be less than max, but more than min!`));
+  });
+
+  it('To < min', () => {
+    const configuration = {
+      min: 0,
+      max: 100,
+      step: 10,
+      currentValueFrom: 20,
+      currentValueTo: -70,
+      isVertical: false,
+      hasInterval: true,
+      hasPointer: true,
+      hasScale: true,
+    };
+
+    expect( function(){ new Model(configuration); } ).toThrow(new Error(`CurrentValueTo must be less than max, but more than min!`));
+  });
+
+  it('To > max', () => {
+    const configuration = {
+      min: 0,
+      max: 100,
+      step: 10,
+      currentValueFrom: 20,
+      currentValueTo: 170,
+      isVertical: false,
+      hasInterval: true,
+      hasPointer: true,
+      hasScale: true,
+    };
+
+    expect( function(){ new Model(configuration); } ).toThrow(new Error(`CurrentValueTo must be less than max, but more than min!`));
+  });
+
+  it('To < from', () => {
+    const configuration = {
+      min: 0,
+      max: 100,
+      step: 10,
+      currentValueFrom: 20,
+      currentValueTo: 5,
+      isVertical: false,
+      hasInterval: true,
+      hasPointer: true,
+      hasScale: true,
+    };
+
+    expect( function(){ new Model(configuration); } ).toThrow(new Error(`CurrentValueTo must be more than currentValueFrom!`));
+  });
+});
+
 describe('Set min value', () => {
   let min: number;
   let max: number;
@@ -90,5 +252,379 @@ describe('Set min value', () => {
     expect(from).toEqual(100);
     expect(to).toEqual(100);
     expect(step).toEqual(0);
+  });
+});
+
+describe('Set max value', () => {
+  let min: number;
+  let max: number;
+  let from: number;
+  let to: number;
+  let step: number;
+
+  const model = new Model(testСonfiguration);
+  const minObserver = (value: number): void => { min = value; };
+  const maxObserver = (value: number): void => { max = value; };
+  const fromObserver = (value: number): void => { from = value; };
+  const toObserver = (value: number): void => { to = value; };
+  const stepObserver = (value: number): void => { step = value; };
+
+  model.attachMin(minObserver);
+  model.attachMax(maxObserver);
+  model.attachValueFrom(fromObserver);
+  model.attachValueTo(toObserver);
+  model.attachStep(stepObserver);
+
+  it('Max > to > from > min', () => {
+    model.setMax(150);
+
+    expect(min).toEqual(0);
+    expect(max).toEqual(150);
+    expect(from).toEqual(20);
+    expect(to).toEqual(70);
+    expect(step).toEqual(10);
+  });
+
+  it('To > max > from > min', () => {
+    model.setMax(67);
+
+    expect(min).toEqual(0);
+    expect(max).toEqual(67);
+    expect(from).toEqual(20);
+    expect(to).toEqual(67);
+    expect(step).toEqual(10);
+  });
+
+  it('To > from > max > min', () => {
+    model.setMax(17);
+
+    expect(min).toEqual(0);
+    expect(max).toEqual(17);
+    expect(from).toEqual(17);
+    expect(to).toEqual(17);
+    expect(step).toEqual(10);
+  });
+
+  it('To > from > max> min and max - min < step', () => {
+    model.setMax(7);
+
+    expect(min).toEqual(0);
+    expect(max).toEqual(7);
+    expect(from).toEqual(7);
+    expect(to).toEqual(7);
+    expect(step).toEqual(7);
+  });
+});
+
+describe('Set step value', () => {
+  let min: number;
+  let max: number;
+  let from: number;
+  let to: number;
+  let step: number;
+
+  const model = new Model(testСonfiguration);
+  const minObserver = (value: number): void => { min = value; };
+  const maxObserver = (value: number): void => { max = value; };
+  const fromObserver = (value: number): void => { from = value; };
+  const toObserver = (value: number): void => { to = value; };
+  const stepObserver = (value: number): void => { step = value; };
+
+  model.attachMin(minObserver);
+  model.attachMax(maxObserver);
+  model.attachValueFrom(fromObserver);
+  model.attachValueTo(toObserver);
+  model.attachStep(stepObserver);
+
+  it('Step < max - min', () => {
+    model.setStep(50);
+
+    expect(min).toEqual(0);
+    expect(max).toEqual(100);
+    expect(from).toEqual(20);
+    expect(to).toEqual(70);
+    expect(step).toEqual(50);
+  });
+
+  it('Step > max - min', () => {
+    model.setStep(250);
+
+    expect(min).toEqual(0);
+    expect(max).toEqual(100);
+    expect(from).toEqual(20);
+    expect(to).toEqual(70);
+    expect(step).toEqual(100);
+  });
+
+  it('Step = 0', () => {
+    expect( function(){ model.setStep(0); } ).toThrow(new Error(`Step must be > 0!`));
+  });
+
+  it('Step < 0', () => {
+    expect( function(){ model.setStep(-5); } ).toThrow(new Error(`Step must be > 0!`));
+  });
+});
+
+describe('Set from value', () => {
+  let min: number;
+  let max: number;
+  let from: number;
+  let to: number;
+  let step: number;
+  let isInterval: boolean;
+
+  const model = new Model(testСonfiguration);
+  const minObserver = (value: number): void => { min = value; };
+  const maxObserver = (value: number): void => { max = value; };
+  const fromObserver = (value: number): void => { from = value; };
+  const toObserver = (value: number): void => { to = value; };
+  const stepObserver = (value: number): void => { step = value; };
+  const intervalObserver = (value: boolean): void => { isInterval = value; };
+
+  model.attachMin(minObserver);
+  model.attachMax(maxObserver);
+  model.attachValueFrom(fromObserver);
+  model.attachValueTo(toObserver);
+  model.attachStep(stepObserver);
+  model.attachInterval(intervalObserver);
+
+  it('To value exist and  from < to', () => {
+    model.setInterval(true);
+    model.setFrom(10);
+
+    expect(isInterval).toEqual(true);
+    expect(min).toEqual(0);
+    expect(max).toEqual(100);
+    expect(from).toEqual(10);
+    expect(to).toEqual(70);
+    expect(step).toEqual(10);
+  });
+
+  it('To value exist and  from > max', () => {
+    model.setInterval(true);
+    model.setFrom(149);
+
+    expect(isInterval).toEqual(true);
+    expect(min).toEqual(0);
+    expect(max).toEqual(100);
+    expect(from).toEqual(70);
+    expect(to).toEqual(70);
+    expect(step).toEqual(10);
+  });
+
+  it('To value does not exist and  from < max', () => {
+    model.setInterval(false);
+    model.setFrom(10);
+
+    expect(isInterval).toEqual(false);
+    expect(min).toEqual(0);
+    expect(max).toEqual(100);
+    expect(from).toEqual(10);
+    expect(to).toEqual(70);
+    expect(step).toEqual(10);
+  });
+
+  it('To value does not exist and  from > max', () => {
+    model.setInterval(false);
+    model.setFrom(150);
+
+    expect(isInterval).toEqual(false);
+    expect(min).toEqual(0);
+    expect(max).toEqual(100);
+    expect(from).toEqual(100);
+    expect(to).toEqual(70);
+    expect(step).toEqual(10);
+  });
+
+  it('To value does not exist, step = 15, set from 18', () => {
+    model.setInterval(false);
+    model.setStep(15);
+    model.setFrom(18);
+
+    expect(isInterval).toEqual(false);
+    expect(min).toEqual(0);
+    expect(max).toEqual(100);
+    expect(from).toEqual(15);
+    expect(to).toEqual(70);
+    expect(step).toEqual(15);
+  });
+
+  it('To value does not exist, step = 15, set from 25', () => {
+    model.setInterval(false);
+    model.setStep(15);
+    model.setFrom(25);
+
+    expect(isInterval).toEqual(false);
+    expect(min).toEqual(0);
+    expect(max).toEqual(100);
+    expect(from).toEqual(30);
+    expect(to).toEqual(70);
+    expect(step).toEqual(15);
+  });
+});
+
+describe('Set to value', () => {
+  let min: number;
+  let max: number;
+  let from: number;
+  let to: number;
+  let step: number;
+  let isInterval: boolean;
+
+  const model = new Model(testСonfiguration);
+  const minObserver = (value: number): void => { min = value; };
+  const maxObserver = (value: number): void => { max = value; };
+  const fromObserver = (value: number): void => { from = value; };
+  const toObserver = (value: number): void => { to = value; };
+  const stepObserver = (value: number): void => { step = value; };
+  const intervalObserver = (value: boolean): void => { isInterval = value; };
+
+  model.attachMin(minObserver);
+  model.attachMax(maxObserver);
+  model.attachValueFrom(fromObserver);
+  model.attachValueTo(toObserver);
+  model.attachStep(stepObserver);
+  model.attachInterval(intervalObserver);
+
+  it('From < to < max', () => {
+    model.setInterval(true);
+    model.setTo(80);
+
+    expect(isInterval).toEqual(true);
+    expect(min).toEqual(0);
+    expect(max).toEqual(100);
+    expect(from).toEqual(20);
+    expect(to).toEqual(80);
+    expect(step).toEqual(10);
+  });
+
+  it('To < from < max', () => {
+    model.setInterval(true);
+    model.setTo(14);
+
+    expect(isInterval).toEqual(true);
+    expect(min).toEqual(0);
+    expect(max).toEqual(100);
+    expect(from).toEqual(20);
+    expect(to).toEqual(20);
+    expect(step).toEqual(10);
+  });
+
+  it('From < max < To', () => {
+    model.setInterval(true);
+    model.setTo(150);
+
+    expect(isInterval).toEqual(true);
+    expect(min).toEqual(0);
+    expect(max).toEqual(100);
+    expect(from).toEqual(20);
+    expect(to).toEqual(100);
+    expect(step).toEqual(10);
+  });
+
+  it('Set to 30, step = 25', () => {
+    model.setInterval(true);
+    model.setStep(25);
+    model.setTo(30);
+
+    expect(isInterval).toEqual(true);
+    expect(min).toEqual(0);
+    expect(max).toEqual(100);
+    expect(from).toEqual(20);
+    expect(to).toEqual(25);
+    expect(step).toEqual(25);
+  });
+
+  it('Set to 40, step = 25', () => {
+    model.setInterval(true);
+    model.setStep(25);
+    model.setTo(40);
+
+    expect(isInterval).toEqual(true);
+    expect(min).toEqual(0);
+    expect(max).toEqual(100);
+    expect(from).toEqual(20);
+    expect(to).toEqual(50);
+    expect(step).toEqual(25);
+  });
+});
+
+describe('Set interval', () => {
+  let isInterval: boolean;
+  const model = new Model(testСonfiguration);
+  const intervalObserver = (value: boolean): void => { isInterval = value; };
+
+  model.attachInterval(intervalObserver);
+
+  it('set interval slider', () => {
+    model.setInterval(true);
+
+    expect(isInterval).toEqual(true);
+  });
+
+  it('set single slider', () => {
+    model.setInterval(false);
+
+    expect(isInterval).toEqual(false);
+  });
+});
+
+describe('Set orientation', () => {
+  let isVertical: boolean;
+  const model = new Model(testСonfiguration);
+  const orientationObserver = (value: boolean): void => { isVertical = value; };
+
+  model.attachOrientation(orientationObserver);
+
+  it('Set horizontal orientation', () => {
+    model.setVertical(false);
+
+    expect(isVertical).toEqual(false);
+  });
+
+  it('Set vertical orientation', () => {
+    model.setVertical(true);
+
+    expect(isVertical).toEqual(true);
+  });
+});
+
+describe('Set pointer', () => {
+  let hasPointer: boolean;
+  const model = new Model(testСonfiguration);
+  const pointerObserver = (value: boolean): void => { hasPointer = value; };
+
+  model.attachPointer(pointerObserver);
+
+  it('Without pointer', () => {
+    model.setPointer(false);
+
+    expect(hasPointer).toEqual(false);
+  });
+
+  it('With pointer', () => {
+    model.setPointer(true);
+
+    expect(hasPointer).toEqual(true);
+  });
+});
+
+describe('Set scale', () => {
+  let hasScale: boolean;
+  const model = new Model(testСonfiguration);
+  const scaleObserver = (value: boolean): void => { hasScale = value; };
+
+  model.attachScale(scaleObserver);
+
+  it('Without pointer', () => {
+    model.setScale(false);
+
+    expect(hasScale).toEqual(false);
+  });
+
+  it('With pointer', () => {
+    model.setScale(true);
+
+    expect(hasScale).toEqual(true);
   });
 });
