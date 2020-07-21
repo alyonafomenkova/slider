361da6dcec7e26a4428188ad1e604c91086da15a
Alena Fomenkova <fruitella30sep@mail.ru>
2020-07-21 17:42:19 +0300

    Extension of jquery interface.

---
+++ src/plugin/jquery-interfaces.ts
@@ -0,0 +1,5 @@
+interface JQuery {
+  // TODO: any убрать, заменить на Configuration и поправить plugin.ts
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  sliderPlugin(options: any): JQuery;
+}
