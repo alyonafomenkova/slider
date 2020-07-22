abd1cc7dc8e79131956da84b23163d8ea6ec2aed
Alena Fomenkova <fruitella30sep@mail.ru>
2020-07-22 18:54:07 +0300

    Init pointer view in presenter.

--- src/plugin/presenter/Presenter.ts
+++ src/plugin/presenter/Presenter.ts
@@ -1,17 +1,24 @@
 import Model from '../model/Model';
 import SliderView from '../view/SliderView';
+import PointerView from '../view/PointerView';
 
 class Presenter {
   private model: Model;
 
   private sliderView?: SliderView;
 
+  private pointerFromView?: PointerView;
+
+  private pointerToView?: PointerView;
+
   constructor(model: Model) {
     this.model = model;
   }
 
-  init(sliderView: SliderView): void {
+  init(sliderView: SliderView, pointerFromView: PointerView, pointerToView: PointerView): void {
     this.sliderView = sliderView;
+    this.pointerFromView = pointerFromView;
+    this.pointerToView = pointerToView;
 
     if (this.model.configuration.isVertical) {
       this.sliderView.clear();
@@ -20,15 +27,14 @@ class Presenter {
       this.sliderView.clear();
       this.sliderView.drawHorizontal();
     }
-  }
 
-  // foo(): void {
-  //   if (this.sliderView) {
-  //     this.sliderView.drawVertical();
-  //   } else {
-  //     throw Error('Slider view not assigned'); // К собеседованию: без new
-  //   }
-  // }
+    this.pointerFromView.drawPointer();
+    console.log('Pointer from отрисован!'); // DEBUG
+    if (this.model.configuration.hasValue) {
+      this.pointerFromView.drawValue();
+      console.log('Value from отрисован!'); // DEBUG
+    }
+  }
 }
 
 export default Presenter;
