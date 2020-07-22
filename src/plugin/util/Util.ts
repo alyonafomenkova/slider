fec9eee646010374bfb73dfd276972ba8f09e2c8
Alena Fomenkova <fruitella30sep@mail.ru>
2020-07-22 18:50:19 +0300

    Adding createElement method.

--- src/plugin/util/Util.ts
+++ src/plugin/util/Util.ts
@@ -2,6 +2,15 @@ class Util {
   static addClassName(element: Element, className: string): void {
     element.classList.add(className);
   }
+
+  static createElement(container: Element, classNames: string, template?: string): void {
+    const element = document.createElement('div');
+    element.className = classNames;
+    if (template) {
+      element.innerHTML = template;
+    }
+    container.append(element);
+  }
 }
 
 export default Util;
