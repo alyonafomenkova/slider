f002f8ebc00e6195cb6aede90d72db43d142fe50
Alena Fomenkova <fruitella30sep@mail.ru>
2020-08-09 19:59:52 +0300

    Updating of panel.

--- src/components/settings-panel/settings-panel.ts
+++ src/components/settings-panel/settings-panel.ts
@@ -1,8 +1,32 @@
 import './settings-panel.scss';
 
 class SettingsPanel {
-  constructor(private panel: HTMLElement) {
+  private panel: Element;
+
+  private inputFrom?: Element;
+
+  private fromValueListener?: (value: number) => void;
+
+  constructor(panel: Element) {
     this.panel = panel;
+  }
+
+  init(): void {
+    this.inputFrom = this.panel.querySelector('.settings-panel__values-input--from') as Element;
+    this.inputFrom.addEventListener('input', this.handleFromInput);
+  }
+
+  private handleFromInput = (evt: Event): void => {
+    const { value } = evt.target as HTMLInputElement;
+    const num = parseFloat(value);
+
+    if (!Number.isNaN(num) && this.fromValueListener) {
+      this.fromValueListener(num);
+    }
+  };
+
+  setFromValueListener(listener: (value: number) => void): void {
+    this.fromValueListener = listener;
   }
 }
 
