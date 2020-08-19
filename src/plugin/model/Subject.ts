c121d056a866afb66c5d2e05e0567a2fb6a42e31
Alena Fomenkova <fruitella30sep@mail.ru>
2020-08-19 22:11:16 +0300

    Changing the access modifiers for the Subject.

--- src/plugin/model/Subject.ts
+++ src/plugin/model/Subject.ts
@@ -7,18 +7,18 @@ class Subject<T> {
     this.value = value;
   }
 
-  getValue(): T {
+  public getValue(): T {
     return this.value;
   }
 
-  setValue(value: T): void {
+  public setValue(value: T): void {
     this.value = value;
     this.observers.forEach((observer) => {
       observer(value);
     });
   }
 
-  attach(observer: (value: T) => void): void {
+  public attach(observer: (value: T) => void): void {
     if (this.observers.includes(observer)) {
       // eslint-disable-next-line no-console
       console.log('Observer already attached.');
@@ -28,7 +28,7 @@ class Subject<T> {
     }
   }
 
-  detach(observer: (value: T) => void): void {
+  public detach(observer: (value: T) => void): void {
     const index = this.observers.indexOf(observer);
 
     if (index === -1) {
