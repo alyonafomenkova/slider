94f0aa10c7c0045ab6150c0866d7329a426c981a
Alena Fomenkova <fruitella30sep@mail.ru>
2020-08-26 12:48:32 +0300

    Updating tests for slider view.

--- src/plugin/tests/view/SliderViewImplTest.ts
+++ src/plugin/tests/view/SliderViewImplTest.ts
@@ -2,7 +2,7 @@ import SliderView from '../../view/SliderView';
 import SliderViewImpl from '../../view/impl/SliderViewImpl';
 
 const containerClass = 'container';
-const setupContainer = (): Element => {
+const setupContainer = (): HTMLElement => {
   const body = document.querySelector('body');
   const container = document.createElement('div');
   container.className = containerClass;
@@ -16,11 +16,12 @@ const setupContainer = (): Element => {
 };
 
 describe('Test slider view implementation', () => {
+  let container: HTMLElement;
   let view: SliderView;
 
   beforeEach(() => {
     // Arrange
-    const container = setupContainer();
+    container = setupContainer();
     view = new SliderViewImpl(container);
   });
 
@@ -53,5 +54,46 @@ describe('Test slider view implementation', () => {
     expect(document.querySelector('.slider__bar--vertical')).toBeNull();
     expect(document.querySelector('.slider--horizontal')).toBeNull();
     expect(document.querySelector('.slider__progress')).toBeNull();
+  });
+
+  it('Set size, bound, click listener. Check horizontal / vertical progress', () => {
+    // Act -> assert
+    container.style.width = '500px';
+    container.style.height = '50px';
+    container.style.left = '8px';
+    container.style.right = '508px';
+    container.style.top = '8px';
+    container.style.bottom = '58px';
+
+    expect(view.getWidth()).toEqual(500);
+    expect(view.getHeight()).toEqual(50);
+    expect(view.getBoundLeft()).toEqual(8);
+    expect(view.getBoundRight()).toEqual(508);
+    expect(view.getBoundTop()).toEqual(8);
+    expect(view.getBoundBottom()).toEqual(58);
+
+    view.drawHorizontal();
+    view.drawHorizontalProgress(8, 500);
+
+    expect(container.querySelector('.slider__progress')).not.toBeNull();
+
+    view.clear();
+    view.drawVertical();
+    view.drawVerticalProgress(58, 50);
+
+    expect(container.querySelector('.slider__progress')).not.toBeNull();
+
+    let valueX = -1;
+    let valueY = -1;
+    view.setClickSliderBarListener((v: SliderView, x: number, y: number) => {
+      valueX = x;
+      valueY = y;
+    });
+
+    const progress = document.querySelector('.slider__progress');
+
+    (progress as HTMLElement).click();
+    expect(valueX).toEqual(0);
+    expect(valueY).toEqual(0);
   });
 });
